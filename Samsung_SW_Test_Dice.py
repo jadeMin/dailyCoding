b56fb4b89f324555e89ee0f04686aa42aad98371
Sun Min Lee <30864676+sunminkr@users.noreply.github.com>
2019-07-20 23:31:41 +0900

    Create Samsung_SW_Test_Dice.py

---
+++ Samsung_SW_Test_Dice.py
@@ -0,0 +1,10 @@
+N, M, x, y, K = input().split()
+board = []
+
+#2-Dimension input as list type
+for i in range(0,N):
+  temp = []
+  for j in range(0,M):
+    temp.append(input().split())
+  board.append(temp)
+  
